83b0ab12a82b162e47142b51d6792ee00474ecf3
apupneja <apupneja2002@gmail.com>
2022-08-01 20:20:48 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{ useEffect } from 'react';
 
 function App() {
+  const [inputValue, setInputValue] = React.useState("");
+  const [spaceBarEnter, setSpaceBarEnter] = React.useState(0);
+
+  useEffect(()=> {
+    if(spaceBarEnter===1){
+      getTransliteration(inputValue) 
+      setSpaceBarEnter(0);
+    }
+  }, [spaceBarEnter])
+
+  const getTransliteration =(val)=>{
+    let arr = val.split(' ')
+    val = arr[arr.length-1]
+    if(val =='.'){
+      return;
+    }
+    const url = `https://inputtools.google.com/request?text=${val}&itc=hi-t-i0-und&num=5&cp=0&cs=1&ie=utf-8&oe=utf-8&app=demopage`;
+    fetch(url).then((response) => response.json())
+              .then(function(data) {
+                let arr = inputValue.split(' ');
+                console.log(data)
+                arr[arr.length-1] = data[1][0][1][0];
+                setInputValue(arr.join(' ')+' ');
+              })
+              .catch((error) => console.log(error));
+  };
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleKeyPress = (e) => {
+    console.log(e.key)
+    if (e.key === " " ||
+      e.code === "Space" ||      
+      e.keyCode === 32   ||
+      e.code === "Enter"
+    ){
+      setSpaceBarEnter(1);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <textarea
+            type="text"
+            value={inputValue}
+            onChange={handleInputChange}
+            onKeyDown={handleKeyPress}
+            className = "textbox"
+          />
       </header>
     </div>
   );
