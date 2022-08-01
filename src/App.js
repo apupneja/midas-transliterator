96a1c15da4fade62caccc02b8270f37fa30ba2f4
apupneja <apupneja2002@gmail.com>
2022-08-02 00:41:04 +0530

    added suggestions

--- src/App.js
+++ src/App.js
@@ -4,24 +4,27 @@ import React,{ useEffect } from 'react';
 function App() {
   const [inputValue, setInputValue] = React.useState("");
   const [spaceBarEnter, setSpaceBarEnter] = React.useState(0);
+  const [suggestions, setSuggestions] = React.useState([]);
 
   const getTransliteration =(val)=>{
     let arr = val.split(' ')
     val = arr[arr.length-1]
     if(val ==='.'){
+      arr[arr.length-1]='|'
       return;
     }
     const url = `https://inputtools.google.com/request?text=${val}&itc=hi-t-i0-und&num=5&cp=0&cs=1&ie=utf-8&oe=utf-8&app=demopage`;
     fetch(url).then((response) => response.json())
               .then(function(data) {
                 let arr = inputValue.split(' ');
-                console.log(data)
+                //console.log(data)
+                setSuggestions(data[1][0][1]);
                 arr[arr.length-1] = data[1][0][1][0];
                 setInputValue(arr.join(' ')+' ');
               })
               .catch((error) => console.log(error));
   };
-  
+
   useEffect(()=> {
     if(spaceBarEnter===1){
       getTransliteration(inputValue);
@@ -34,7 +37,7 @@ function App() {
   };
 
   const handleKeyPress = (e) => {
-    console.log(e.key)
+    //console.log(e.key)
     if (e.key === " " ||
       e.code === "Space" ||      
       e.keyCode === 32   ||
@@ -44,9 +47,24 @@ function App() {
     }
   }
 
+  const clickHandler = (event, val) =>{
+    let arr = inputValue.split(' ');
+    //console.log(arr)
+    //console.log(arr.length-2)
+    arr[arr.length-2] = suggestions[val];
+    setInputValue(arr.join(' '));
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+          <div className='buttonGroup'>
+            <button className='button' onClick={event => clickHandler(event,0)}>{suggestions[0]}</button>
+            <button className='button' onClick={event => clickHandler(event,1)}>{suggestions[1]}</button>
+            <button className='button' onClick={event => clickHandler(event,2)}>{suggestions[2]}</button>
+            <button className='button' onClick={event => clickHandler(event,3)}>{suggestions[3]}</button>
+            <button className='button' onClick={event => clickHandler(event,4)}>{suggestions[4]}</button>
+          </div>
           <textarea
             type="text"
             value={inputValue}
